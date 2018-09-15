1877d85f8bae9c6934fa11ac80ce6b8923da8925
DESKTOP-RHGID0I\pooch <poocha316@gmail.com>
2018-09-15 14:04:10 +0200

    functions1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-asdasd
+
+
+def minutes_to_hours(minutes):
+    hours = minutes / 60
+    print(hours)
+
+def condition1(a,b):
+    if a < b:
+        print(str(a) + " lower than " + str(b))
+    else:
+        print(str(a) + " higher than " + str(b))
+
+condition1(160,30)
